683afe1d6cb1c9544ba299e137264c8effadbf58
vueliwei <32789331+vueliwei@users.noreply.github.com>
2019-10-09 17:01:58 +0800

    Add files via upload

---
+++ pages/center/center.vue
@@ -0,0 +1,52 @@
+<template>
+	<view>
+		<view class="logo" @click="goLogin" :hover-class="!login ? 'logo-hover' : ''">
+			<image class="logo-img" :src="login ? uerInfo.avatarUrl :avatarUrl"></image>
+			<view class="logo-title">
+				<text class="uer-name">Hi，{{login ? uerInfo.name : '您未登录'}}</text>
+				<text class="go-login navigat-arrow" v-if="!login">&#xe65e;</text>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	/*export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+	*/
+	export default {
+		data() {
+			return {
+				login: false,
+				avatarUrl: '/static/logo.png',
+				uerInfo: {}
+			}
+		},
+		methods: {
+			goLogin() {
+				if (!this.login) {
+					uni.navigateTo({
+						url: '/pages/login/login'
+					});
+				}
+			},
+			goAbout() {
+				uni.navigateTo({
+					url: '/pages/about/about'
+				});
+			}
+		}
+	}
+</script>
+
+<style>
+
+</style>
